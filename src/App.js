224c86bf956ed80ba99ab543d95dbffa56859244
Happiness ojo <48913853+hapojo@users.noreply.github.com>
2022-10-27 13:49:05 +0100

    profile_img

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
             <img src="./image/shareIcon.svg" alt=""></img>
           </div>
         </div>
+
+        <div class="profile">
+          <img src="./image/profile1.svg" class="image1" id="profile_img" alt=""></img>
+          <div class="profileMiddle">
+            <div class="profileIcon">
+              <img src="./image/Icon (3).svg" class="image2" alt=""></img>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
