39fc3596059eaf53579a0c15ef7206c71d4d07b1
Happiness ojo <48913853+hapojo@users.noreply.github.com>
2022-10-27 14:17:10 +0100

    button

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
 
         <h4>Annette Black</h4>
 
+        <div class="social-handle">
+          <button id="twitter"><a href="">Twitter link</a></button>
+          <button id="btn_zuri"><a href="https://training.zuri.team/">Zuri Team</a></button>
+          <button id="books"><a href=" http://books.zuri.team">Zuri Books</a></button>
+          <button id="book_python"><a href="https://books.zuri.team/">Python Books</a></button>
+          <button id="pitch"><a href=" https://background.zuri.team">Background Check for Coder</a></button>
+          <button id="book_design"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
+        </div>
+
       </div>
     </div>
   );
