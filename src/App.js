2ef386219615c5a842a38e52b6b0dbd294c84eda
Happiness ojo <48913853+hapojo@users.noreply.github.com>
2022-10-27 13:26:15 +0100

    share icon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='container'></div>
+      <div className='container'>
+        <div class="share">
+          <div class="shareIcon">
+            <img src="./image/shareIcon.svg" alt=""></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
